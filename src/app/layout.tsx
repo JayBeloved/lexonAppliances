4301b3120c750c4bc85bbc20ba3e0d379a2cb6a4
John J. Lawal <contact.johnjlawal@gmail.com>
2025-11-26 08:38:21 +0000

    SEO Optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,30 @@ import Footer from "@/components/layout/Footer";
 export const metadata: Metadata = {
   title: "Lexon Appliance",
   description: "Premium kitchen and home appliances at pocket-friendly prices.",
+  icons: {
+    icon: "/lexon_logo.jpg",
+  },
+  keywords: [
+    "Lexon",
+    "appliances",
+    "kitchen",
+    "home",
+    "premium",
+    "affordable",
+    "cheap appliances",
+    "Nigeria appliances",
+    "affordable appliances in Nigeria",
+    "Lexon appliances in Nigeria",
+    "Lexon appliances in Lagos",
+    "Lexon appliances in Abuja",
+    "Lexon appliances in Port Harcourt"
+  ],
+  authors: [
+    {
+      name: "Lexon",
+      url: "https://www.lexonappliances.com",
+    },
+  ],
 };
 
 export default function RootLayout({
@@ -18,7 +42,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap"
           rel="stylesheet"
